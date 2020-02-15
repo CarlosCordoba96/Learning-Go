6e9d470d0f26e296bd38f410b22f88e014d8c061
CarlosCordoba96 <Carlos.Cordoba@alu.uclm.es>
2020-02-15 23:14:45 +0100

    Formatting code and testing gitlens for vscode

--- 01-Basic Operations/Variables/hello.go
+++ 01-Basic Operations/Variables/hello.go
@@ -5,6 +5,8 @@ import "fmt"
 func main() {
     fmt.Println("hello world")
     for i := 0; i < 10; i++ {
-        fmt.Println(i)
+    if i%2==0 {
+        fmt.Println("El numero",i," es par")
+    }
     }
 }
